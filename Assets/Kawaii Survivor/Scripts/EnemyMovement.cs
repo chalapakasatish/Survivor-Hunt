7cb874f6cca28808e4704858beb18a3fc67c5038
chalapakasatish <chalapakasatish@gmail.com>
2024-05-28 08:49:35 +0530

    Enemy Complete

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -8,6 +8,15 @@ public class EnemyMovement : MonoBehaviour
     private Player player;
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+    [Header("Spawn Sequence Related")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    private bool hasSpawned;
+    [Header("Debug")]
+    [SerializeField] private bool gizmos;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +25,55 @@ public class EnemyMovement : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        renderer.enabled = false;
+        spawnIndicator.enabled = true;
+
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject,targetScale,.3f).setLoopPingPong(4).setOnComplete(SpawnSequenceCompleted);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!hasSpawned)
+            return;
+
+        FollowPlayer();
+        TryAttack();
+    }
+    private void SpawnSequenceCompleted()
+    {
+        renderer.enabled = true;
+        spawnIndicator.enabled = false;
+        hasSpawned = true;
+    }
+    private void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - transform.position).normalized;
 
-        Vector2 targetPosition = (Vector2)transform.position  + direction * moveSpeed * Time.deltaTime;
+        Vector2 targetPosition = (Vector2)transform.position + moveSpeed * Time.deltaTime * direction;
 
-        transform.position = targetPosition;    
+        transform.position = targetPosition;
+    }
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer <= playerDetectionRadius)
+        {
+            PassAway();
+        }
+    }
+    private void PassAway()
+    {
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        if (!gizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
